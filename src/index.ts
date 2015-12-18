9407bc31d4579d1990a64256a81ba26ef6549940
mizdra <pp.mizdra@gmail.com>
2015-12-18 19:32:55 +0900

    index.jsをtsに書き換え

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 'use asm';
 
 import PokeData from '@mizdra/gen3-poke-data';
-let json = require('./pokedex.json');
+const json = require('./pokedex.json');
 
 const pokedex = json.map(d => new PokeData(d));
 export default pokedex;
