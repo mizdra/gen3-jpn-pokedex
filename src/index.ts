7c37a138c629493b40476c7f685d037a01f4c0a3
mizdra <pp.mizdra@gmail.com>
2015-12-20 01:16:27 +0900

    JSONSchemaに従ってjsonを読み込むように変更

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /// <reference path='../typings/bundle.d.ts'/>
 
-import {PokeData} from '@mizdra/gen3-poke-data';
-const json: any[] = require('./pokedex.json');
+import {PokeData, JSONSchema} from '@mizdra/gen3-poke-data';
+const json: JSONSchema[] = require('./pokedex.json');
 
 const pokedex = json.map(d => new PokeData(d));
 export default pokedex;
